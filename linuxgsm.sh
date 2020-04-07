9e33c925520db28835cfa4d2da118c848f7d26ce
make2019 <50690441+make2019@users.noreply.github.com>
2020-04-07 17:45:34 +0800

    test
    test new user

--- linuxgsm.sh
+++ linuxgsm.sh
@@ -44,7 +44,7 @@ userinput="${1}"
 ## GitHub Branch Select
 # Allows for the use of different function files
 # from a different repo and/or branch.
-githubuser="GameServerManagers"
+githubuser="make2019"
 githubrepo="LinuxGSM"
 githubbranch="master"
 
